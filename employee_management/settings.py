a1674e939fec7cab5a92f725b7dba322fcd8d4c0
DoRenato <b4g4c31r0@gmail.com>
2021-10-22 17:56:20 -0300

    Tentando corrigir erro h10 heroku

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost:8000', 'management-alliance.herokuapp.com', "https://management-alliance.herokuapp.com/"]
+ALLOWED_HOSTS = ['localhost:8000', 'management-alliance.herokuapp.com']
 
 
 # Application definition
