e4177bb425d765e02aec37ef846e497b5b8834d8
DoRenato <b4g4c31r0@gmail.com>
2021-10-22 18:08:18 -0300

    Nova app no heroku.

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost:8000', 'management-alliance.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'management-emp.herokuapp.com']
 
 
 # Application definition
