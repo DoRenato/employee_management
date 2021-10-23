c3b2269dd4170fe99f87f781434d28fb6b222878
DoRenato <b4g4c31r0@gmail.com>
2021-10-23 09:33:58 -0300

    Terceira app criada, ainda na tentativa de corrigir erro.

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES = { 
-    'default': config(
-        'DATABASE_URL', 
-        default=default_dburl, 
-        cast=dburl
-    ), 
+DATABASES = {
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 # DATABASES = {
 #     'default': {
